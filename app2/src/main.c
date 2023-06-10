26f098d379d48a585239e7643dbd33ddc230ada0
BaardSagen <bard.sagstad@saivas.com>
2023-06-10 09:29:22 +0200

    first

---
+++ app2/src/main.c
@@ -0,0 +1,8 @@
+#include <app_version.h>
+#include <zephyr/kernel.h>
+
+int main(void) {
+    printk("Application version %s initialized on board %s!\r\n",
+           APP_VERSION_STRING, CONFIG_BOARD);
+    return 0;
+}
